400b8118ae56768306b60985d44a32d05d621dca
xe3qrh44r <liulin063@gmail.com>
2023-07-20 10:49:36 +0800

    Update Footer.js
    Remove title

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -28,7 +28,6 @@ const Footer = ({ siteInfo }) => {
                 <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
             <span className='pl-2 hidden busuanzi_container_site_uv'>
                 <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-            <h1 className='pt-1'>{siteInfo?.title}</h1>
 
         </footer>
   )
