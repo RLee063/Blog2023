6689a7d8180b603dc2997f215c0023bc3db7fcfc
xe3qrh44r <liulin063@gmail.com>
2023-07-29 14:32:24 +0800

    Update BlogPostCard.js

--- themes/gitbook/components/BlogPostCard.js
+++ themes/gitbook/components/BlogPostCard.js
@@ -7,7 +7,7 @@ const BlogPostCard = ({ post, className }) => {
   const router = useRouter()
   const currentSelected = router.asPath.split('?')[0] === '/' + post.slug
   return (
-        <div key={post.id} className={`${className} py-1 cursor-pointer px-2 dark: text-green-500`}>
+        <div key={post.id} className={`${className} py-1 cursor-pointer px-2 dark: ${currentSelected ? 'text-green-500' : ''}`}>
             <div className="flex flex-col w-full select-none">
                 <Link href={`${BLOG.SUB_PATH}/${post.slug}`} passHref>
                     {post.title}
